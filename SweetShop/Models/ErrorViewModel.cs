07f0926e3bb72957bae8ac2eb5fded1760801508
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2023-02-22 22:17:07 +0200

    Create project

---
+++ SweetShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SweetShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
