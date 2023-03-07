18a4490658d8192dc83c813e9f22154713264a58
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2023-03-07 23:56:54 +0200

    Add toast for notifications

---
+++ SweetShop/Constants/NotificationsConstants.cs
@@ -0,0 +1,12 @@
+﻿namespace SweetShop.Constants
+{
+    public class NotificationsConstants
+    {
+        public const string SUCCESS_NOTIFICATION = "Success";
+        public const string ERROR_NOTIFICATION = "Error";
+        public const string WARNING_NOTIFICATION = "Warning";
+        public const string INFORMATION_NOTIFICATION = "Info";
+
+
+    }
+}
