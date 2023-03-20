31ed61772bdba9b018e687d69995d87a5d75df4c
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2023-03-20 22:13:39 +0200

    Fix validations

--- SweetShop/Constants/ModelConstants/AllergenConstants.cs
+++ SweetShop/Constants/ModelConstants/AllergenConstants.cs
@@ -3,9 +3,9 @@
     public class AllergenConstants
     {
         public const int NAME_MIN_VALUE = 3;
-        public const int NAME_MAX_VALUE = 3;
+        public const int NAME_MAX_VALUE = 20;
 
         public const int DESCRIPTION_MIN_VALUE = 10;
-        public const int DESCRIPTION_MAX_VALUE = 255;
+        public const int DESCRIPTION_MAX_VALUE = 1000;
     }
 }
--- SweetShop/Constants/ModelConstants/ProductConstants.cs
+++ SweetShop/Constants/ModelConstants/ProductConstants.cs
@@ -6,7 +6,7 @@
         public const int NAME_MAX_VALUE = 30;
 
         public const int DESCRIPTION_MIN_VALUE = 10;
-        public const int DESCRIPTION_MAX_VALUE = 255;
+        public const int DESCRIPTION_MAX_VALUE = 1000;
 
         public const double PRICE_MIN_VALUE = 0.01;
         public const double PRICE_MAX_VALUE = 1000;
