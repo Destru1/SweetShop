982a070e8f0c5d799f42612a74560adf6fef060d
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2023-03-23 13:11:53 +0200

    Update review views

--- SweetShop/Constants/ModelConstants/ReviewConstants.cs
+++ SweetShop/Constants/ModelConstants/ReviewConstants.cs
@@ -3,7 +3,7 @@
     public class ReviewConstants
     {
         public const int DESCRIPTION_MIN_VALUE = 10;
-        public const int DESCRIPTION_MAX_VALUE = 255;
+        public const int DESCRIPTION_MAX_VALUE = 1000;
 
         public const int RATING_MIN_VALUE = 1;
         public const int RATING_MAX_VALUE = 10;
