abfc09b5ec713c15caec2329d885e3438f6c196b
Nikolay Nikolaev <nikolay.p.nikolaev01@gmail.com>
2023-03-24 19:25:39 +0200

    Changes in review service.

--- SweetShop/Areas/Store/Controllers/ReviewsController.cs
+++ SweetShop/Areas/Store/Controllers/ReviewsController.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using SweetShop.Areas.Store.Controllers;
 using SweetShop.Constants;
@@ -37,25 +38,27 @@ namespace SweetShop.Controllers
 
 
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Index(string sort)
         {
+            ViewData["HighestRated"] = sort == "int" ? "top" : "int";
+            ViewData["LowestRated"] = sort == "int" ? "low" : "int";
+
+           
             var reviews = this.reviewService.GetAll();
+
+            if (sort == "top")
+            {
+                reviews = reviews.OrderByDescending(r => r.Rating);
+                
+            }
+            if (sort == "low")
+            {
+                reviews = reviews.Where(r => r.Rating < 5).ToList();
+            }
 
             return this.View(reviews);
         }
 
-        //[HttpGet]
-        //public IActionResult Details(int id)
-        //{
-        //    var review = this.reviewService.GetDetails(id);
-
-        //    if (review == null)
-        //    {
-        //        return this.RedirectToAction("Index");
-        //    }
-
-        //    return this.View(review);
-        //}
 
         [Authorize(Roles = RolesConstants.CLIENT_ROLE)]
         [HttpGet]
@@ -86,43 +89,6 @@ namespace SweetShop.Controllers
             return this.RedirectToAction("Index");
         }
 
-        //[HttpGet]
-        //public IActionResult Update(int id)
-        //{
-        //    var reviewToUpdate = this.reviewService.GetById<OrderDTO>(id);
-
-        //    IEnumerable<ProductIndexViewModel> products = this.productService.GetAll();
-        //    IEnumerable<ClientIndexViewModel> clients = this.clientService.GetAll();
-
-        //    if (!this.ModelState.IsValid)
-        //    {
-        //        return this.RedirectToAction("Index");
-
-        //    }
-
-        //    this.ViewBag.Products = products;
-        //    this.ViewBag.Clients = clients;
-
-        //    return this.View(reviewToUpdate);
-        //}
-
-        //[HttpPost]
-        //public async Task<IActionResult> Update(int id, ReviewDTO order)
-        //{
-        //    if (!this.ModelState.IsValid)
-        //    {
-        //        return this.View(order);
-
-        //    }
-        //    var isUpdated = await this.reviewService.UpdateAsync(id, order);
-
-        //    if (!isUpdated)
-        //    {
-        //        return this.RedirectToAction("Index", "Home");
-        //    }
-
-        //    return this.RedirectToAction("Index");
-        //}
 
         [Authorize(Roles = RolesConstants.ADMIN_ROLE)]
         [HttpGet]
--- SweetShop/Services/ReviewService.cs
+++ SweetShop/Services/ReviewService.cs
@@ -20,7 +20,6 @@ namespace SweetShop.Services.Interfaces
         }
 
 
-
         public IEnumerable<IndexReviewViewModel> GetAll()
         {
             var reviews = this.DbContext.Reviews.ProjectTo<IndexReviewViewModel>(this.Mapper.ConfigurationProvider).ToList();
@@ -42,24 +41,6 @@ namespace SweetShop.Services.Interfaces
             return reviewToReturn;
         }
 
-        //public DetailsReviewViewModel GetDetails(int id)
-        //{
-        //    var review = this.DbContext.Reviews.Where(x => x.Id == id)
-        //        .Select(x => new DetailsReviewViewModel
-        //        {
-        //            Id = x.Id,
-        //            Product = x.Product.Name,
-        //            Client = x.Client.FirstName + " " + x.Client.LastName,
-        //            Rating = x.Rating,
-        //            Description = x.Description,
-        //            CreatedOn = x.CreatedOn,
-        //            ModifiedOn = x.ModifiedOn,
-        //        })
-        //        .FirstOrDefault();
-
-        //    return review;
-
-        //}
 
         public async Task CreateAsync(ReviewDTO review)
         {
@@ -78,27 +59,6 @@ namespace SweetShop.Services.Interfaces
             await this.DbContext.AddAsync(reviewToCreate);
             await this.DbContext.SaveChangesAsync();
         }
-
-        //public async Task<bool> UpdateAsync(int id, ReviewDTO review)
-        //{
-        //    var reviewToUpdate = this.DbContext.Reviews.Find(review.Id);
-
-        //    if (review == null)
-        //    {
-        //        return false;
-        //    }
-
-        //    reviewToUpdate.Description = review.Description;
-        //    reviewToUpdate.Rating = review.Rating;
-        //    reviewToUpdate.ClientId = review.ClientId;
-        //    reviewToUpdate.ProductId = review.ProductId;
-        //    reviewToUpdate.ModifiedOn = DateTime.UtcNow;
-
-        //    this.DbContext.Update(reviewToUpdate);
-        //    await this.DbContext.SaveChangesAsync();
-
-        //    return true;
-        //}
 
         public async Task<bool> DeleteAsync(int id)
         {
